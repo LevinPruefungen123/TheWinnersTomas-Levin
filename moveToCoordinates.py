a49e980e84843462e53f09e9777d59d96dfc8e39
LevinPruefungen123 <levinlinder7@gmail.com>
2024-11-14 12:03:46 +0100

    Changed buyIron.py to buyRessources.py and added Gold purchaseing/selling feature

--- moveToCoordinates.py
+++ moveToCoordinates.py
@@ -1,9 +1,9 @@
 import requests
 
 url = "http://192.168.100.13:2009/set_target"
-payload = {"target": {"x": 150000, "y": 150000}} # Hier koordinaten Editieren um an einen X-Beliebigen Ort zu gehen.
+payload = {"target": {"x": -20100, "y": 11300}} # Hier koordinaten Editieren um an einen X-Beliebigen Ort zu gehen.
 
-def set_target(payload):
+def set_target(url, payload):
     try:
         response = requests.post(url, json=payload)
         if response.status_code == 200:
@@ -12,3 +12,9 @@ def set_target(payload):
             print(f"Failed to set target to {payload['target']}: {response.text}")
     except requests.RequestException as e:
         print(f"Error setting target: {e}")
+
+def main():
+    set_target(url, payload)
+
+if __name__ == "__main__":
+    main()
